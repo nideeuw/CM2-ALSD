e6ef1f938e1aa1f70e8bae65a48d2b34a82d35a5
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-05-28 15:31:29 +0700

    update

--- KlinikMain.java
+++ KlinikMain.java
@@ -21,8 +21,39 @@ public class KlinikMain {
 
             switch (pilih) {
                 case 1:
+                    System.out.print("Nama Pasien: ");
+                    String nama = sc.nextLine();
+                    System.out.print("NIK: ");
+                    String nik = sc.nextLine();
+                    System.out.print("Keluhan: ");
+                    String keluhan = sc.nextLine();
+                    antrian.tambahAntrian(new Pasien(nama, nik, keluhan));
+                    break;
+
                 case 2:
+                    antrian.tampilkanAntrian();
+                    break;
+
                 case 3:
+                    if (antrian.isEmpty()) {
+                        System.out.println(">> Tidak ada pasien dalam antrian.");
+                    } else {
+                        Pasien p = antrian.layaniPasien();
+                        System.out.println("Pasien " + p.nama + " dipanggil");
+                        System.out.print("Masukkan ID Dokter: ");
+                        String id = sc.nextLine();
+                        System.out.print("Masukkan Nama Dokter: ");
+                        String namaDok = sc.nextLine();
+                        System.out.print("Masukkan Durasi Layanan (jam): ");
+                        int durasi = sc.nextInt();
+                        sc.nextLine();
+                        Dokter d = new Dokter(id, namaDok);
+                        TransaksiLayanan t = new TransaksiLayanan(p, d, durasi);
+                        transaksiQueue.tambahTransaksi(t);
+                        System.out.println(">> Pasien telah dilayani, transaksi berhasil dicatat.");
+                    }
+                    break;
+
                 case 4:
                     System.out.println(">> Sisa pasien dalam antrian: " + antrian.hitungSisaAntrian());
                     break;
